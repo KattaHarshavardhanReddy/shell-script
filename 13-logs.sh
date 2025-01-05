4adbb5633f17c1d34c648ab8598cf043e1d88eaa
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-01-05 22:05:29 +0530

    hello

--- 13-logs.sh
+++ 13-logs.sh
@@ -6,7 +6,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 H="\e[34m"
-Logs_folder="/var/log/shellscript-log"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
 if [ $1 -ne 0 ]
@@ -19,32 +21,34 @@ if [ $1 -ne 0 ]
 
 }
 
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
 if [ $USERID -ne 0 ]
 then
     echo "Error:: u sud have sudo access"
     exit 1 #other than 0
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 
 then 
 
-    dnf install mysql -y
+    dnf install mysql - &>>$LOG_FILE_NAME
 
     VALIDATE $? "installed mysql"
 else
     echo -e " $Y mysql  already installed"
 fi
 
-dnf list installed git
+dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 
 then 
 
-    dnf install git -y
+    dnf install git -y &>>$LOG_FILE_NAME
 
       VALIDATE $? "installed git"
 else
