96df6a0c7fdee01ba36d0ecad9614c9deb40d5d3
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-01-06 13:30:30 +0530

    hi

--- 10-condition.sh
+++ 10-condition.sh
@@ -7,3 +7,13 @@ then
     echo "U need sudo access"
     exit 1
 fi
+
+dnf install mysqll -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation fail"
+    exit 1
+else
+    echo "installation success"
+fi
